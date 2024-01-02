568cba057d533bfeb856f02c3f454997ac0d3ff8
Nakuziri <xcampos@brooklynsteamcenter.org>
2024-01-02 10:53:03 -0500

    finished todo jinja

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,7 +6,12 @@ todos = ["Hang out with friend", "Play on my computer"]
 
 @app.route('/', methods=['GET','POST']) 
 def index():
-    todos = request.form['todos']
-    todos.append(todos)
-    return render_template("todo.html.jinja", todos = todos)
- 
+    if request.method == "POST" :
+        new_todos = request.form['todos']
+        todos.append(new_todos)
+    return render_template("todo.html.jinja", new_todos = todos)
+
+@app.route('/delete_todos/<int:todo_index>', methods=['POST'])
+def todo_dalete(todo_index):
+    del todos[todo_index]
+    return redirect('/')
