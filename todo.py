ba96dc144331aa06a96ee9f2753eecaf91fc3b70
Nakuziri <xcampos@brooklynsteamcenter.org>
2024-01-04 10:54:10 -0500

    worked on sql for todos

--- todo.py
+++ todo.py
@@ -1,4 +1,14 @@
 from flask import Flask, render_template, request, redirect
+import pymysql 
+import pymysql.cursors
+
+conn = pymysql.connect(
+   database="xcampos_todos",
+   user='xcampos',
+   password='221349988',
+   host='10.100.33.60', 
+   cursorclass=pymysql.cursors.DictCursor
+)
 
 app = Flask(__name__)
 
@@ -8,7 +18,12 @@ todos = ["Hang out with friend", "Play on my computer"]
 def index():
     if request.method == "POST" :
         new_todos = request.form['todos']
-        todos.append(new_todos)
+        cursor = conn.cursor()
+        cursor.execute("SELECT `description` FROM `todos`")
+        results = cursor.fetchall()
+        if request.method == "POST":
+            new_todos = request.form[todos]
+            todos.append(new_todos)
     return render_template("todo.html.jinja", new_todos = todos)
 
 @app.route('/delete_todos/<int:todo_index>', methods=['POST'])
