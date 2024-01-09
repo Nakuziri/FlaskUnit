16d97b9c3bdc8a17c2040a0a9b803735456483bf
Nakuziri <xcampos@brooklynsteamcenter.org>
2024-01-09 10:51:07 -0500

    working on todo list functionality

--- todo.py
+++ todo.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import pymysql 
 import pymysql.cursors
+from pprint import pprint as print
 
 conn = pymysql.connect(
    database="xcampos_todos",
@@ -12,21 +13,26 @@ conn = pymysql.connect(
 
 app = Flask(__name__)
 
-todos = ["Hang out with friend", "Play on my computer"]
+# Todos = ["Hang out with friend", "Play on my computer"]
 
 @app.route('/', methods=['GET','POST']) 
 def index():
     if request.method == "POST" :
-        new_todos = request.form['todos']
-        cursor = conn.cursor()
-        cursor.execute("SELECT `description` FROM `todos`")
-        results = cursor.fetchall()
-        if request.method == "POST":
-            new_todos = request.form[todos]
-            todos.append(new_todos)
-    return render_template("todo.html.jinja", new_todos = todos)
+        cursor = conn.cursor() 
+        new_todos = request.form['new_todos']
+        # Todos.append(new_todos)
+        cursor.execute(f"INSERT INTO `Todos`(`description`) VALUES ('{new_todos}')")
+        conn.commit()
+        cursor.close()
+    
+    cursor = conn.cursor()      
+    cursor.execute("SELECT * FROM `Todos`")
+    results = cursor.fetchall()
+    cursor.close()
 
-@app.route('/delete_todos/<int:todo_index>', methods=['POST'])
-def todo_dalete(todo_index):
-    del todos[todo_index]
-    return redirect('/')
+    return render_template("todo.html.jinja", new_todos = results)
+
+# @app.route('/delete_todos/<int:todo_index>', methods=['POST'])
+# def todo_delete(todo_index):
+#     del Todos[todo_index]
+#     return redirect('/')
